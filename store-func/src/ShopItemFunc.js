6f68d9f2802466c4f8be78859d7c2be6d51daef1
Pavel <39239196+Tojefin@users.noreply.github.com>
2022-10-10 13:10:23 +0400

    update

--- store-func/src/ShopItemFunc.js
+++ store-func/src/ShopItemFunc.js
@@ -1,20 +1,26 @@
+import PropTypes from 'prop-types';
+
 export default function ShopItemFunc(props) {
-  let item = props.item;
+  let { item } = props;
 
   return(
-    <div class="main-content">
+    <div className="main-content">
       <h2>{item.brand}</h2>
       <h1>{item.title}</h1>
       <h3>{item.description}</h3>
-      <div class="description">
+      <div className="description">
       {item.descriptionFull}
       </div>
-      <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
-      <div class="divider"></div>
-      <div class="purchase-info">
-        <div class="price">{item.currency}{item.price.toFixed(2)}</div>
+      <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">{item.currency}{item.price.toFixed(2)}</div>
         <button>Добавить в корзину</button>
       </div>
     </div>
   )
+}
+
+ShopItemFunc.propTypes = {
+  item: PropTypes.object
 }
