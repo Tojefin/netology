2e616ac69b3b26bc042894d5e522ee6867f3a7f3
Pavel <39239196+Tojefin@users.noreply.github.com>
2022-10-10 13:13:29 +0400

    update

--- store-class/src/ShopItemClass.js
+++ store-class/src/ShopItemClass.js
@@ -1,27 +1,28 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class ShopItemClass extends Component {
-  constructor(props) {
-    super(props);
-    this.item = this.props.item;
-  }
-  
   render() {
+    const { item } = this.props;
     return(
-      <div class="main-content">
-      <h2>{this.item.brand}</h2>
-      <h1>{this.item.title}</h1>
-      <h3>{this.item.description}</h3>
-      <div class="description">
-      {this.item.descriptionFull}
+      <div className="main-content">
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
+      <div className="description">
+      {item.descriptionFull}
       </div>
-      <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
-      <div class="divider"></div>
-      <div class="purchase-info">
-        <div class="price">{this.item.currency}{this.item.price.toFixed(2)}</div>
+      <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">{item.currency}{item.price.toFixed(2)}</div>
         <button>Добавить в корзину</button>
       </div>
     </div>
     )
   }
+}
+
+ShopItemClass.propTypes = {
+  item: PropTypes.object
 }
